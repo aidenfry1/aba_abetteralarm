ee0d7674d754d390b706debed55fbaf9504b5e49
aiden <aiden.fry@ui-uk.city>
2020-08-29 10:25:28 +0100

    Creation and experimentation for the alarm list layout. - Expanding rows - Toggle on off - List of days

--- lib/activities/main_activity.dart
+++ lib/activities/main_activity.dart
@@ -7,26 +7,14 @@ class MainActivity extends StatefulWidget {
   _MainActivityState createState() => _MainActivityState();
 }
 
-//Widget build(BuildContext context) {
-//  return MaterialApp(
-//    title: 'ABA',
-//    theme: ThemeData(
-//      primarySwatch: Colors.deepPurple,
-//      visualDensity: VisualDensity.adaptivePlatformDensity,
-//    ),
-//  );
-//}
-
 class _MainActivityState extends State<MainActivity> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(S.of(context).appTitleLong),
-        backgroundColor: Color(0xff35149E),
       ),
       body: AlarmList(),
-      backgroundColor: Color(0xff240E6B),
     );
   }
 }
--- lib/views/AlarmList.dart
+++ lib/views/AlarmList.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class AlarmList extends StatefulWidget {
   @override
   _State createState() => _State();
 }
 
-class _State extends State<AlarmList> {
+class _State extends State<AlarmList> with TickerProviderStateMixin {
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
         padding: EdgeInsets.all(8),
         itemBuilder: (context, i) {
-          if (i.isOdd) return Divider();
+          if (i.isOdd)
+            return Divider(
+              color: Color(0xFF4E1EEB),
+            );
 
           final index = i ~/ 2;
 
@@ -24,26 +33,120 @@ class _State extends State<AlarmList> {
   }
 
   Widget _buildRow(AlarmData data) {
-    return AlarmListItem(data: data);
-  }
-}
-
-class AlarmListItem extends StatelessWidget {
-  final AlarmData data;
-  AlarmListItem({this.data});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(data.label),
+    return ExpansionTile(
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(children: <Widget>[
+            Text("6:00", style: myAlarmTextStyle),
+            Switch(
+              value: data.isActive,
+              onChanged: (value) {
+                setState(() {
+                  data.isActive = value;
+                });
+              },
+              inactiveTrackColor: Colors.white30,
+//              activeColor: Colors.green,
+            ),
+          ], mainAxisAlignment: MainAxisAlignment.spaceBetween),
+        ],
+      ),
+      subtitle: Wrap(
+        spacing: 4, // set spacing here
+        children: <Widget>[
+          CircleAvatar(
+            radius: 15,
+            backgroundColor: Color.fromARGB(255, 132, 101, 235),
+            child: CircleAvatar(
+              radius: 14,
+              backgroundColor: Color.fromARGB(255, 36, 14, 107),
+              child: Center(
+                child: Text("M", style: myDaysTextStyle),
+              ),
+            ),
+          ),
+          Container(
+            width: 32,
+            height: 32,
+            decoration: new BoxDecoration(
+              borderRadius: new BorderRadius.all(new Radius.circular(50.0)),
+              border: new Border.all(
+                color: Color.fromARGB(255, 132, 101, 235),
+                width: 1.0,
+              ),
+            ),
+            child: Center(
+              child: Text("T", style: myDaysTextStyle),
+            ),
+          ),
+          Text("T", style: myDaysTextStyle),
+          Text("W", style: myDaysTextStyle),
+          Text("T", style: myDaysTextStyle),
+          Text("F", style: myDaysTextStyle),
+          Text("S", style: myDaysTextStyle),
+          Text("S", style: myDaysTextStyle),
+        ],
+      ),
+      children: <Widget>[
+        Padding(
+            padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text("Sound", style: myDaysTextStyle),
+                    Text("Oxygen", style: myDaysTextStyle)
+                  ],
+                ),
+                Divider(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text("Label", style: myDaysTextStyle),
+                    Text(data.label, style: myDaysTextStyle)
+                  ],
+                ),
+                Divider(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text("Delete", style: myDaysTextStyle),
+                    Text("Skip ", style: myDaysTextStyle),
+                  ],
+                ),
+              ],
+            ))
+      ],
     );
   }
 }
 
+final myAlarmTextStyle = TextStyle(color: Colors.white, fontSize: 24);
+
+final myDaysTextStyle = TextStyle(color: Colors.white, fontSize: 20);
+
 class AlarmData {
   String label;
+  TimeOfDay time;
+  bool repeatMon,
+      repeatTues,
+      repeatWeds,
+      repeatThurs,
+      repeatFri,
+      repeatSat,
+      repeatSun;
+  bool isActive = true;
+  bool isExpanded = true;
+  String sound;
+
   AlarmData({this.label});
 }
 
-List<AlarmData> testListOfAlarms =
-    ({AlarmData(label: "one"), AlarmData(label: "two")}).toList();
+List<AlarmData> testListOfAlarms = ({
+  AlarmData(label: "One"),
+  AlarmData(label: "Two"),
+  AlarmData(label: "Three"),
+  AlarmData(label: "Four"),
+}).toList();
